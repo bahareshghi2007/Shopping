5b8fb81040833b0a7681743a4ebb2bf282150317
Bahar Eshghi <124599947+bahareshghi2007@users.noreply.github.com>
2023-02-13 15:58:28 +0330

    save Products & Cart to LocalStorage

--- js/Products.js
+++ js/Products.js
@@ -1,9 +1,11 @@
+import Storage from './Storage.js';
 const productsContainer = document.querySelector('.products-container');
 const searchInput = document.querySelector('.search-input');
 const sortContainer = document.querySelector('.sort');
 const cartContainer = document.querySelector('.cart-items');
 const cartNumber = document.querySelector('.cart-number');
 let allProducts = [];
+let cart = Storage.getCart();
 
 class Products {
   getProducts() {
@@ -47,11 +49,21 @@ class Products {
       this.sortProducts(e.target.innerText)
     );
 
-    //   addEvent to addToCartBtns:
+    // addToCartBtns:
     const addToCartBtns = document.querySelectorAll('.add-to-cart-btn');
-    addToCartBtns.forEach((btn) =>
-      btn.addEventListener('click', (e) => this.addCartItem(e.target))
-    );
+    addToCartBtns.forEach((btn) => {
+      // addEvent to btn
+      btn.addEventListener('click', (e) => this.addCartItem(e.target));
+
+      // btn DOM:
+      const isInCart = cart.find((item) => item.id == btn.dataset.id);
+      if (isInCart) {
+        btn.innerText = 'In Cart';
+        btn.disabled = true;
+      }
+    });
+    // update LocalStorage:
+    Storage.saveProducts(products);
   }
 
   searchProducts(searchValue) {
@@ -76,25 +88,58 @@ class Products {
     const addedProduct = allProducts.find(
       (product) => product.id == e.dataset.id
     );
+    cart = [...cart, addedProduct];
     this.createCartItem(addedProduct);
-    //   update DOM:
+    // update DOM:
     e.innerText = 'In Cart';
     e.disabled = true;
   }
 
   createCartItem(item) {
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('cart-item');
+    if (item.length == 0) {
+      return;
+    } else if (item.length >= 1) {
+      item.forEach((i) => {
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('cart-item');
 
-    const checkTitleLength =
-      item.title.length > 10 ? 'long title' : 'short title';
+        const checkTitleLength =
+          i.title.length > 10 ? 'long title' : 'short title';
 
-    itemDiv.innerHTML = `
+        itemDiv.innerHTML = `
+        <img src="${i.image}" alt="product-img" class="item-img">
+        <div class="item-desc">
+           <p class="item-title">${i.title.slice(0, 10)} ${
+          checkTitleLength == 'long title' ? `...` : ''
+        }</p>
+           <p class="item-price">${i.price}$</p>
+        </div>
+        <div class="item-right">
+           <div class="item-quantity">
+              <i class="fa-solid fa-chevron-up"></i>
+              <p class="item-number">1</p>
+              <i class="fa-solid fa-chevron-down"></i>
+           </div>
+           <i class="fa-solid fa-trash"></i>
+        </div>
+        `;
+
+        // update DOM:
+        cartContainer.appendChild(itemDiv);
+      });
+    } else {
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('cart-item');
+
+      const checkTitleLength =
+        item.title.length > 10 ? 'long title' : 'short title';
+
+      itemDiv.innerHTML = `
         <img src="${item.image}" alt="product-img" class="item-img">
         <div class="item-desc">
            <p class="item-title">${item.title.slice(0, 10)} ${
-      checkTitleLength == 'long title' ? `...` : ''
-    }</p>
+        checkTitleLength == 'long title' ? `...` : ''
+      }</p>
            <p class="item-price">${item.price}$</p>
         </div>
         <div class="item-right">
@@ -106,9 +151,18 @@ class Products {
            <i class="fa-solid fa-trash"></i>
         </div>
         `;
-    // update DOM:
-    cartContainer.appendChild(itemDiv);
+
+      // update DOM:
+      cartContainer.appendChild(itemDiv);
+    }
+
     cartNumber.innerText = cartContainer.childNodes.length;
+    // update LocalStorage:
+    Storage.saveCart(cart);
+  }
+
+  resetApp() {
+    searchInput.value = '';
   }
 }
 
--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 import CartModal from './CartModal.js';
 import Products from './Products.js';
+import Storage from './Storage.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   //cart Modal:
   CartModal;
   // create Products:
   Products.getProducts();
+  // create CartItems:
+  const cartItems = Storage.getCart();
+  Products.createCartItem(cartItems);
 });
